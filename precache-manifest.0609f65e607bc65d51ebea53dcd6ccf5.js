2f12a888dfdb4caf00631f0836ac611d81710365
joseortiz9 <joseco04@live.com>
2020-01-21 04:10:02 +0300

    Updates

---
+++ precache-manifest.0609f65e607bc65d51ebea53dcd6ccf5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b96387c6ad619b2451d110cc1f39e584",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "0543e9bd9e7319d84e6b",
+    "url": "/monsters-rolodex/static/css/main.298741fa.chunk.css"
+  },
+  {
+    "revision": "3cc81b6ba8721a243f2d",
+    "url": "/monsters-rolodex/static/js/2.e397b8d6.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.e397b8d6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0543e9bd9e7319d84e6b",
+    "url": "/monsters-rolodex/static/js/main.9a5eed7e.chunk.js"
+  },
+  {
+    "revision": "458299b00a849d3ba5e6",
+    "url": "/monsters-rolodex/static/js/runtime-main.ead69200.js"
+  }
+]);
